0eacffc602cdcb520b8e618a6940df4c124e428a
crojas02 <carlos.tardio03@gmail.com>
2023-02-21 19:38:18 -0300

    workflow teste13

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -2,6 +2,7 @@ resource "aws_lambda_function" "executa_emr" {
   # Cluster: EMR-Ney-IGTI-deltaTerminated with errorsThe subnet configuration was invalid: 
   # The subnet subnet-1df20360 does not exist.
   # 48 30
+  # 40 - a subnet foi criado no outro video sobre o emr, copiado e colado, por isso o erro, ver o outro video como se cria a subnet
   filename      = "lambda_function_payload.zip" # este arquivo é criado na nossa esteira
   function_name = var.lambda_function_name
   role          = aws_iam_role.lambda.arn
