829bb090fa7be4a43c70a38a499cbc080b7b3d7c
crojas02 <carlos.tardio03@gmail.com>
2023-02-21 18:10:48 -0300

    primeiro commit

---
+++ infrastructure/s3.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "dl" {
+  bucket = "datalake-carlucho-igti-edc-tf"
+  acl    = "private"
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+
---
+++ infrastructure/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "lambda_function_name" {
+  default = "IGTIexecutaEMRaovivo"
+}
+
